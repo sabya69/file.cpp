a5d38c623baede7ba33b6cfdfe4e8c06c7b6b6ec
Sabyasachi Das Biswas <dasbiswassabyasachi7@gmail.com>
2025-02-21 11:08:46 +0530

    Update basic_pointer.cpp

--- basic_pointer.cpp
+++ basic_pointer.cpp
@@ -25,6 +25,17 @@ int main (){
          cout << " before t " << t << endl;
          t = t + 1;
          cout << " after t " << t << endl;
+         //playing with pointer using array 
+            int arr[] = {10,20,30,40,50};
+            cout << "address of first memory block is " << arr << endl;
+            cout << arr[0] << endl;
+            cout << "address of first memory block is " << &arr[0] << endl;
+            cout << "1st " << *arr << endl;
+            cout << "2nd " << *(arr+1) << endl;
+            cout << "3rd " << *(arr+2) << endl;
+            cout << "4th " << *(arr+3) << endl;
+            cout << "5th " << *(arr+4) << endl;
+            cout << "6th " << *(arr+5) << endl;
      
         return 0;
 }
